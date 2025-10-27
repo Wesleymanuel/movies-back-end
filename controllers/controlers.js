0263481a0fed82090fd3e00b32fb219edf27bc03
FernandoVini <araujofernando592@gmail.com>
2025-10-27 07:55:14 -0300

    implementacao do hash da senha e token jwt do usuario

--- controllers/controlers.js
+++ controllers/controlers.js
@@ -1,17 +1,27 @@
 const pool = require('../db/bd')
+const bcript = require("bcrypt")
+const jwt = require('jsonwebtoken')
+require('dotenv').config()
+
+const SALT = process.env.SALT_PASSOWORD
+const JWT = process.env.JWT_SECRET
 
 
 const controlerrs = {
     mensage : async (req,res) => {
 
-        const {name , email, cpf} = req.body;
+        const {name , email, cpf, senha} = req.body;
 
-        if(!name || !email || !cpf){
+        if(!name || !email || !cpf || !senha){
            return res.status(400).json({msg : "os campos de email e senha sao obrigatorios"})
         }
+
+        const passowordHash = bcript.hashSync(senha,SALT)
+
     try{
-            const query = `INSERT INTO usuarios (nome,email,cpf) VALUES ($1,$2,$3)`
-            const results = await pool.query(query , [name, email, cpf])
+            const query = `INSERT INTO usuarios (nome,email,cpf,senha) VALUES ($1,$2,$3,$4)`
+            const results = await pool.query(query , [name, email, cpf,passowordHash])
+            const token = jwt.sign({id : user.id }, JWT , { expiresIn : "1d" })
             res.status(201).json({msg : "usuarios cadastrado com sucesso" , user: results.rows[0]})
         }
     catch(error){
@@ -37,7 +47,43 @@ const controlerrs = {
         res.status(500).json({msg : "erro no servidor"})
     }
 
-}
+},
+
+    login : async (req,res) => {
+        const { name, senha, email } = req.body;
+
+        if(!name || !senha){
+           return res.status(400).json({msg : "os campos de email e senha sao obrigatorios"})
+        }
+
+        try{
+            const query = `SELECT id,senha FROM usuarios WHERE email = $1`
+            const results = await pool.query(query , [email])
+            const user = results.rows[0]
+
+            if (!user) {
+                 return res.status(404).json({ msg: 'Usuário não encontrado' });
+            }
+
+            const passorwordCompare = bcript.compare(senha, user.senha)
+
+            if(!passorwordCompare){
+                return res.status(401).json({msg : "senha incorreta"})
+            }
+
+            jwt.sign({id : user.id}, JWT , {expiresIn : "1d"})
+            
+                res.status(200).json({
+                        msg: 'Login realizado com sucesso',
+                        user: { id: user.id, nome: user.nome, email: user.email },
+                        token,
+                    });
+            
+        }
+        catch(error){
+
+        }
+    }
 }
 
 module.exports = controlerrs
