503e38840bde0666b0484a5dc724e85b9d7ce059
Wesleymanuel <wesleyemanuel0510@gmail.com>
2025-11-05 13:02:26 -0300

    criacao da rota de atualizacao de senha

--- controllers/controlers.js
+++ controllers/controlers.js
@@ -27,40 +27,6 @@ const controlerrs = {
     }
 },
 
-
-    exclusao : async (req,res) => {
-        const {name} = req.body;
-
-        try{
-        if(!name){
-            return res.status(400).json({msg : "os campos sao obrigatorios para deletar"})
-        }
-        const query = `DELETE FROM usuarios WHERE nome = $1`
-        await pool.query(query,[name])
-        res.status(201).json({msg : "usuarios deletado com sucesso"})
-    }
-    catch(error){
-        console.log(error)
-        res.status(500).json({msg : "erro no servidor"})
-    }
-
-},
-
-
-    atualizacao : async (req,res) => {
-
-        const { name } = req.body;
-
-        try{
-            const query = `UPDATE usuarios SET nome = $1  WHERE id = 1`
-            await pool.query(query, [name])
-            res.status(201).json({msg : "nome atualizado com sucesso"})
-        }
-        catch(error){
-            console.log(error)
-            res.status(500).json({msg : "erro no servidor"})
-    }
-},
     login : async (req,res) => {
         const { email,senha } = req.body;
 
@@ -97,7 +63,43 @@ const controlerrs = {
         catch(error){
             console.log(error)
         }
-    }
+    },
+
+    atualizacao : async (req,res) => {
+
+      const  { email,senha } = req.body; 
+
+        if(!email){
+           return res.status(400).json({msg : "o campos de email e obrigatorio"})
+        }
+
+      try{
+        const query = `SELECT id,senha FROM usuarios WHERE email = ($1)`
+        const results = await pool.query(query,[email])
+        const user = results.rows[0];
+
+        if(!user){
+           return res.status(401).json({msg : "usuario nao encontrado"})
+        }
+
+        const newPassowordHash = await bcrypt.hash(senha,10)
+
+        if(!newPassowordHash){
+           return res.status(500).json({msg : "problemas no servidor, tente novamente"})
+        }
+
+        const newQuery = `UPDATE usuarios SET senha = ($1) WHERE id = ($2)`
+        await pool.query(newQuery,[newPassowordHash,user.id])
+
+        res.status(200).json({msg : "senha renovada com sucesso"})
+
+      }catch(error){
+        console.log(error)
+      }
+    },
+
+
 }
+
 
 module.exports = controlerrs
